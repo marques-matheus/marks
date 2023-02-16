fc35dd5226be130719894f459baf7391ce53b5ed
Matheus Marques <90071675+marques-matheus@users.noreply.github.com>
2023-02-16 16:50:16 -0300

    banner

---
+++ src/components/Atoms/Banner/Banner.tsx
@@ -0,0 +1,11 @@
+
+export default function Banner() {
+    return (
+        <section >
+                <img src="/home.jpg" alt="" className="w-full h-2/4 " />
+
+        </section>
+    
+        );
+
+}
