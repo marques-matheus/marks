44df75e1e7abbdbb9f1776cf7adc45fb8ded9230
Matheus Marques <90071675+marques-matheus@users.noreply.github.com>
2024-11-06 15:43:11 -0300

    Update NavLink.tsx

--- src/components/Atoms/Navlink/NavLink.tsx
+++ src/components/Atoms/Navlink/NavLink.tsx
@@ -13,7 +13,7 @@ export default function NavLink({
       <Link
         href={href}
         target={external ? "_blank" : ""}
-        className="text-marks-500 hover:text-marks-100  hover:border-b-2 hover:border-marks-100 transition-all px-3 py-2 text-md mx-1 font-light uppercase"
+        className="text-marks-700  hover:border-b-2 hover:border-marks-100 hover:text-marks-100 hover:bg-gradient-to-r hover:from-teal-50 hover:to-white transition-all px-3 py-2 text-md mx-1 font-normal uppercase"
       >
         {text}
       </Link>
